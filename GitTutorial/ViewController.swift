e5a4edd1deaf43380e39ba11d2e59e743ff74409
Ani Karapetyan <akarapetyan@lp016-macbook-air.local>
2018-02-01 16:57:27 +0400

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Ani Karapetyan on 2/1/18.
+//  Copyright © 2018 Ani Karapetyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
